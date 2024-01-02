9fd9f84c6a6b58571d966b7f33f64f860b3be519
faizaungguh <otemoos@outlook.com>
2024-01-02 18:58:49 +0700

    int

--- main.go
+++ main.go
@@ -3,5 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Satu adalah ", 1)
+	fmt.Println("Dua adalah ", 2)
+	fmt.Println("Tiga koma lima adalah ", 3.5)
 }
